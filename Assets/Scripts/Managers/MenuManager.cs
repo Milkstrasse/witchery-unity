68ade554c5fd5c72da2c8185fafcd568cefac82b
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2024-10-29 14:26:47 +0100

    Added player info to MenuScene

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -3,45 +3,10 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private RectTransform mainMenu;
-    [SerializeField] private RectTransform modeMenu;
-
-    [SerializeField] private Button joinButton;
-    [SerializeField] private Button hostButton;
-
     private void Start()
     {
         GlobalManager.QuitAnyConnection();
         GlobalManager.singleton.joincode = "";
-
-        joinButton.interactable = GlobalManager.singleton.isConnected;
-        hostButton.interactable = GlobalManager.singleton.isConnected;
-
-        if (GlobalManager.singleton.maxPlayers > 0)
-        {
-            mainMenu.localPosition = new Vector3(-mainMenu.sizeDelta.x * 1.5f - 20f, mainMenu.localPosition.y, mainMenu.localPosition.z);
-            modeMenu.localPosition = new Vector3(-mainMenu.sizeDelta.x * 0.5f, modeMenu.localPosition.y, modeMenu.localPosition.z);
-        }
-    }
-
-    public void SwitchToModeMenu()
-    {
-        AudioManager.singleton.PlayStandardSound();
-
-        GlobalManager.singleton.maxPlayers = 5;
-
-        LeanTween.moveLocalX(mainMenu.gameObject, -mainMenu.sizeDelta.x * 1.5f - 20f, 0.3f);
-        LeanTween.moveLocalX(modeMenu.gameObject, -mainMenu.sizeDelta.x * 0.5f, 0.3f);
-    }
-
-    public void SwitchToMainMenu()
-    {
-        AudioManager.singleton.PlayStandardSound();
-        
-        GlobalManager.singleton.maxPlayers = 0;
-
-        LeanTween.moveLocalX(mainMenu.gameObject, -mainMenu.sizeDelta.x * 0.5f, 0.3f);
-        LeanTween.moveLocalX(modeMenu.gameObject, mainMenu.sizeDelta.x * 0.5f + 20f, 0.3f);
     }
 
     public void SetJoincode(string joincode)
@@ -83,5 +48,10 @@ public class MenuManager : MonoBehaviour
     {
         AudioManager.singleton.PlayStandardSound();
         GlobalManager.singleton.LoadScene(scene);
+    }
+
+    public void DeletePlayer()
+    {
+        SaveManager.DeleteData();
     }
 }
---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playerName;
+    [SerializeField] private TextMeshProUGUI money;
+
+    [SerializeField] private RectTransform mainMenu;
+    [SerializeField] private RectTransform modeMenu;
+
+    [SerializeField] private Button joinButton;
+    [SerializeField] private Button hostButton;
+
+    private void Start()
+    {
+        playerName.text = GlobalSettings.playerName;
+        money.text = $"{GlobalSettings.money} SP";
+
+        joinButton.interactable = GlobalManager.singleton.isConnected;
+        hostButton.interactable = GlobalManager.singleton.isConnected;
+
+        if (GlobalManager.singleton.maxPlayers > 0)
+        {
+            mainMenu.localPosition = new Vector3(-mainMenu.sizeDelta.x * 1.5f - 20f, mainMenu.localPosition.y, mainMenu.localPosition.z);
+            modeMenu.localPosition = new Vector3(-mainMenu.sizeDelta.x * 0.5f, modeMenu.localPosition.y, modeMenu.localPosition.z);
+        }
+    }
+
+    public void SwitchToModeMenu()
+    {
+        AudioManager.singleton.PlayStandardSound();
+
+        GlobalManager.singleton.maxPlayers = 5;
+
+        LeanTween.moveLocalX(mainMenu.gameObject, -mainMenu.sizeDelta.x * 1.5f - 20f, 0.3f);
+        LeanTween.moveLocalX(modeMenu.gameObject, -mainMenu.sizeDelta.x * 0.5f, 0.3f);
+    }
+
+    public void SwitchToMainMenu()
+    {
+        AudioManager.singleton.PlayStandardSound();
+        
+        GlobalManager.singleton.maxPlayers = 0;
+
+        LeanTween.moveLocalX(mainMenu.gameObject, -mainMenu.sizeDelta.x * 0.5f, 0.3f);
+        LeanTween.moveLocalX(modeMenu.gameObject, mainMenu.sizeDelta.x * 0.5f + 20f, 0.3f);
+    }
+}
