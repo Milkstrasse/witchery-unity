d32a5d32bcc21f6e71007717c2174093764883c3
Milkstrasse <55480379+Milkstrasse@users.noreply.github.com>
2025-04-09 21:18:59 +0200

    Improved CPU logic

--- Assets/Scripts/GameData/CPULogic.cs
+++ Assets/Scripts/GameData/CPULogic.cs
@@ -38,17 +38,19 @@ public struct CPULogic
                 }
 
                 int health = move.health;
-                if (logic.lastCard.card.hasMove && logic.lastCard.card.move.moveID == 7)
-                {
-                    health = 0;
-                }
-                else if (move.moveID >= 2 && move.moveID <= 4 && move.moveType == MoveType.Special) //special moves
+                if (move.moveID >= 2 && move.moveID <= 4 && move.moveType == MoveType.Special) //special moves
                 {
                     health *= logic.lastCard.card.hasMove ? logic.lastCard.card.move.cost : 0;
                 }
 
                 if (health < 0) //all attacks
                 {
+                    if (logic.lastCard.card.hasMove && logic.lastCard.card.move.moveID == 7)
+                    {
+                        prioritizedCards.Add((i, -15 + player.cardHand[i].move.cost)); //get biggest amount of resources back
+                        continue;
+                    }
+
                     health = Math.Min(health - logic.players[1].GetPowerBonus() + logic.players[0].GetDamageModifier(), 0);
 
                     if (logic.players[0].health + health <= 0) //opponent defeated
@@ -85,7 +87,7 @@ public struct CPULogic
                                 goto default;
                             }
                         case 3: // heal
-                            if (health > 0 && missingHP > 0.6)
+                            if (health > 0 && missingHP > 0.6f)
                             {
                                 health = Math.Max(health + logic.players[1].GetPowerBonus(), 0);
 
@@ -108,6 +110,11 @@ public struct CPULogic
                                     prioritizedCards.Add((i, health));
                                 }
 
+                                break;
+                            }
+                            else if (missingHP < 0.1f) //excessive healing
+                            {
+                                prioritizedCards.Add((i, -15 + player.cardHand[i].move.cost)); //get biggest amount of resources back
                                 break;
                             }
                             else
@@ -298,85 +305,4 @@ public struct CPULogic
 
         return new MoveMessage(1, cards[0].Item1, cards[0].Item2 > -10);
     }
-
-    /*
-    static func getCardIndex(player: Player, opponent: Player, playedCard: PlayedCard?, cardToPlay: Bool) -> (Int, Bool) {
-        var cards: [SelectedFighter] = []
-
-        for index in player.cardHand.indices {
-            var card: Card = player.cardHand[index]
-
-            if playedCard != nil {
-                if card.spell.type == 12 || (cardToPlay && card.spell.type == 10) { //copy card or reflect card
-                    card = playedCard!.card
-                }
-            }
-
-            if player.mana >= card.spell.cost {
-                var spellDamage: Int = card.spell.damage[1]
-                switch card.spell.type {
-                case 3: //hex
-                    spellDamage *= player.hexes.count * 4
-                case 4: //card
-                    spellDamage *= (playedCard?.card.spell.cost ?? 0) * 4
-                default:
-                    break
-                }
-
-                var healAmount: Int = card.spell.damage[0]
-                switch card.spell.type {
-                case 3: //hex
-                    healAmount *= player.hexes.count * 4
-                case 4: //card
-                    healAmount *= (playedCard?.card.spell.cost ?? 0) * 4
-                default:
-                    break
-                }
-
-                if player.getDamage(damage: spellDamage) >= opponent.currHealth + opponent.getShields() { //defeat opponent
-                    return (index, true)
-                } else if card.spell.type == 9 && playedCard != nil && !playedCard!.played && playedCard!.card.spell.damage[1] > 0 { //block damage
-                    cards.append((index, 100))
-                } else if card.spell.type == 10 && playedCard != nil && !playedCard!.played && playedCard!.card.spell.hex[1] != nil { //reflect hex
-                    cards.append((index, 100))
-                } else if Float(player.currHealth)/Float(player.maxHealth) < 0.35 && (card.spell.damage[0] < 0 || (card.spell.hex[0] == Hexes.healing && !player.hasHex(hex: Hexes.healing))) { //heal
-                    cards.append((index, 80 - healAmount))
-                } else if card.spell.type == 17 && player.curses > 0 { //cleanse
-                    cards.append((index, 50))
-                } else {
-                    var priority: Int = player.getDamage(damage: spellDamage)
-                    if card.spell.hex[0] != nil || card.spell.hex[1] != nil {
-                        if card.spell.hex[1] != Hexes.bomb || !opponent.hasHex(hex: Hexes.bomb) {
-                            priority += 1
-                        }
-                    }
-
-                    cards.append((index, priority))
-                }
-            }
-        }
-
-        cards.sort { $0.1 > $1.1 }
-
-        let cardIndex: Int = cards.first?.0 ?? Int.random(in: 0 ..< player.cardHand.count)
-
-        var card: Card = player.cardHand[cardIndex]
-
-        if player.mana >= card.spell.cost || player.cardHand[cardIndex].spell.type == 13 {
-            if cardIndex == player.cardHand.count - 1 && player.critCounter == 0 && card.spell.type < 5 && card.spell.mana[0] > 0 { //no need to waste crit to gain mana if no other cards are left
-                return (cardIndex, false)
-            } else if card.spell.type == 11 && player.currHealth >= opponent.currHealth { //don't give opponent health back
-                return (cardIndex, false)
-            } else if card.spell.type == 6 && player.hasPositiveHexes() { //don't remove good hexes
-                return (cardIndex, false)
-            } else if card.spell.type == 7 && !opponent.hasPositiveHexes() { //don't get negative hexes
-                return (cardIndex, false)
-            } else {
-                return (cardIndex, true)
-            }
-        } else {
-            return (cardIndex, false)
-        }
-    }
-    */
 }
